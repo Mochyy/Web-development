8abbc4dea234c6d7cc23a7384701b201a06af82c
DiemMy2909 <73978578+DiemMy2909@users.noreply.github.com>
2021-01-21 11:14:43 +0700

    update

--- Java/entity/ProductEntity.java
+++ Java/entity/ProductEntity.java
@@ -20,7 +20,9 @@ public class ProductEntity {
         ArrayList<Product> result = new ArrayList<Product>();
         try (Connection con = ConnectionDB.open()) {
 
+//đổi giá thành 0 - 200 thôi m ý là nó chạy giá thấp hơn á 0 20 30 đồ
 
+            // dung limit ngắn k,
             String sql = "SELECT * FROM product  ORDER BY product." + sort + " asc LIMIT ?,?";
             //String sql = "SELECT * FROM product1  ORDER BY product1.Price asc LIMIT 0,10";
 
@@ -51,16 +53,25 @@ public class ProductEntity {
 
             PreparedStatement ps = con.prepareStatement(sql);
 
+            // có làm cái giá k
+            //làm đi :v
+            // cái mục bên cạch có mỗi cái giá k ak k lm size vs màu
+            //ừa đúng rồi, làm mỗi giá thôi thâu cái giá bữa nào t lm chỉ cho h mò cái kia ,
+            // giá thì chác thêm 1 tham số nữa
 
 //            String sql2 = "SELECT  *  from product p "
 
             int start = (nowpage - 1) * sosp1trang;
 
+            // ok hỉu
 
             // cái limit(start,end) nó bắt đàu lấy từ start, rồi lấy bao nhiêu (end)
             System.out.println(start);
             System.out.println(sosp1trang);
 
+            // mé k biết s nó load tầm bậy
+            // thôi có gi tham khảo cái limit á gọn hơn nhiều
+            // t làm cách này xog hôm sau thầy ns sài cái limit ::((
 
 //            ps.setString(1,sort);
             ps.setInt(1, start);
@@ -72,13 +83,13 @@ public class ProductEntity {
                 result.add(new Product(
                         rs.getString(1),
                         rs.getString(2),
-                        rs.getString(3),
-                        rs.getDouble(4),
+                        getImg(rs.getString(1)),
+                        rs.getDouble(3),
+                        rs.getString(4),
                         rs.getString(5),
-                        rs.getString(6),
-                        rs.getInt(7),
-                        rs.getString(8),
-                        rs.getString(9)
+                        rs.getInt(6),
+                        rs.getString(7),
+                        rs.getString(8)
 
                 ));
 
@@ -93,6 +104,58 @@ public class ProductEntity {
             return result;
         }
 
+    }
+
+    public String getImg(String id) {
+        String resutl = "";
+        try (Connection con = ConnectionDB.open()) {
+            String sql = "select img_1 from img where id=?";
+
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1, id);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                resutl = rs.getString(1);
+            } else
+                resutl = "";
+            ps.close();
+            rs.close();
+            con.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return resutl;
+    }
+
+    public List<Product> getByCaterogy(String cate) {
+        List<Product> list = new ArrayList<>();
+        try (Connection con = ConnectionDB.open()) {
+            String sql = "select * from product where id_loai=?";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1, cate);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                list.add(new Product(
+                        rs.getString(1),
+                        rs.getString(2),
+                        getImg(rs.getString(1)),
+                        rs.getDouble(3),
+                        rs.getString(4),
+                        rs.getString(5),
+                        rs.getInt(6),
+                        rs.getString(7),
+                        rs.getString(8)
+
+                ));
+            }
+            rs.close();
+            ps.close();
+            con.close();
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return list;
     }
 
     public int getNumberPage() {
@@ -113,7 +176,6 @@ public class ProductEntity {
             while (rs.next()) {
                 p.setId(rs.getString(1));
                 p.setName(rs.getString(2));
-                p.setImg(rs.getString(3));
                 p.setPrice(rs.getDouble(4));
             }
             rs.close();
@@ -125,131 +187,29 @@ public class ProductEntity {
 
     }
 
-    public boolean addProduct(String id, String name, String img, String price, String category) {
-        if (!checkID(id)) {
-            try (Connection connection = ConnectionDB.open()) {
-                String sql = "INSERT INTO product (id, name, img, price,state,idCategory ) VALUES (?,?,?,?,'Còn hàng',?)";
 
-                PreparedStatement s = connection.prepareStatement(sql);
-
-                s.setString(1, id);
-                s.setString(2, name);
-                s.setString(3, img);
-                s.setString(4, price);
-                s.setString(5, category);
-                s.executeUpdate();
-                s.close();
-                return true;
-            } catch (SQLException e) {
-                Logger.getLogger(ProductEntity.class.getName()).log(Level.SEVERE, null, e);
-            }
-
-
-        } else {
-            return false;
-        }
-        return false;
-
-    }
-    //
-    public Product getProduct(String id) {
-        PreparedStatement s = null;
-//        Connection con = ConnectionDB.getConn();
-        Product p = new Product();
-        try {
-            String sql = "select * from product where id=?";
-//            PreparedStatement s = con.prepareStatement(sql);
-            s = ConnectionDB.connect(sql);
-            s.setString(1, id);
-            ResultSet rs = s.executeQuery();
-            while (rs.next()) {
-                p.setId(rs.getString(1));
-                p.setName(rs.getString(2));
-                p.setImg(rs.getString(3));
-                p.setPrice(rs.getDouble(4));
-                p.setIdCategory(rs.getString("idCategory"));
-            }
-            rs.close();
-            s.close();
-        } catch (SQLException | ClassNotFoundException e) {
-            Logger.getLogger(ProductEntity.class.getName()).log(Level.SEVERE, null, e);
-        }
-        return p;
-
-    }
-    //update san pham
-    public boolean updateProduct(String id, String name, String img, String price, String category) {
-
-        try (Connection connection = ConnectionDB.open()) {
-            String sql = "UPDATE product SET name = ?,img = ?, price = ?,idCategory = ? WHERE id = ?";
-
-            PreparedStatement s = connection.prepareStatement(sql);
-
-            s.setString(5, id);
-            s.setString(1, name);
-            s.setString(2, img);
-            s.setString(3, price);
-            s.setString(4, category);
-            s.executeUpdate();
-            s.close();
-            return true;
-        } catch (SQLException e) {
-            Logger.getLogger(ProductEntity.class.getName()).log(Level.SEVERE, null, e);
-        }
-
-
-        return false;
-
-    }
-
-    //kiểm tra id đã tồn tại hay chưa
-    public boolean checkID(String id) {
-        boolean check = false;
-
-        PreparedStatement s = null;
-//        Connection con = ConnectionDB.getConn();
-        Product p = new Product();
-        try {
-            String sql = "select * from product where id=?";
-//            PreparedStatement s = con.prepareStatement(sql);
-            s = ConnectionDB.connect(sql);
-            s.setString(1, id);
-            ResultSet rs = s.executeQuery();
-            if (rs.next()) return check = true;
-            rs.close();
-            s.close();
-        } catch (SQLException | ClassNotFoundException e) {
-            Logger.getLogger(ProductEntity.class.getName()).log(Level.SEVERE, null, e);
-        }
-
-
-        return check;
-    }
-
-
-    public int innerAll(Collection<Product> data) {
-        Statement s = null;
-        try {
-            s = ConnectionDB.connect();
-            String sql = "INSERT INTO product (id, name, img, price) VALUES ";
-            int i = 0;
-            for (Product d : data) {
-                if (i < data.size() - 1)
-                    sql += "(\"" + d.getId() + "\",\"" + d.getName() + "\",\"" + d.getImg() + "\"," + d.getPrice() + "),";
-                else
-                    sql += "(\"" + d.getId() + "\",\"" + d.getName() + "\",\"" + d.getImg() + "\"," + d.getPrice() + ")";
-                i++;
-            }
-//            System.out.println(sql);
-            s.executeUpdate(sql);
-            s.close();
-            return 0;
-        } catch (SQLException | ClassNotFoundException throwables) {
-            throwables.printStackTrace();
-            return 0;
-        }
-    }
-
+    //    public int innerAll(Collection<Product> data) {
+//        Statement s = null;
+//        try {
+//            s = ConnectionDB.connect();
+//            String sql = "INSERT INTO producttt (id, name, img, price) VALUES ";
+//            int i = 0;
+//            for (Product d : data) {
+//                if (i < data.size()-1)
+//                    sql += "(\"" + d.getId() + "\",\"" + d.getName() + "\",\"" + d.getImg() + "\"," + d.getPrice()  + "),";
+//                else
+//                    sql += "(\"" + d.getId() + "\",\"" + d.getName() + "\",\"" + d.getImg() + "\"," + d.getPrice() + ")";
+//                i++;
+//            }
+////            System.out.println(sql);
+//            s.executeUpdate(sql);
+//            s.close();
+//            return 0;
+//        } catch (SQLException | ClassNotFoundException throwables) {
+//            throwables.printStackTrace();
+//            return 0;
+//        }
+//    }
     public List<Product> getListByPage(List<Product> arr, int start, int end) {
         List<Product> listPage = new LinkedList<>();
         for (int i = start; i < end; i++) {
@@ -259,7 +219,7 @@ public class ProductEntity {
     }
 
 
-    //    public List<Product> getListProductByCategory(String idCategory ){
+//    public List<Product> getListProductByCategory(String idCategory ){
 //        PreparedStatement s = null;
 //        List<Product> list = new LinkedList<>();
 //        try {
@@ -290,42 +250,42 @@ public class ProductEntity {
 //        }
 //        return list;
 //    }
-    public List<Product> getAll() {
-        List<Product> arr = new ArrayList<>();
-        try (Connection connection = ConnectionDB.open()) {
-            String sql = "select * from product";
-            PreparedStatement ps = connection.prepareStatement(sql);
-            ResultSet rs = ps.executeQuery();
-            while (rs.next()) {
-                arr.add(new Product(
-                        rs.getString(1),
-                        rs.getString(2),
-                        rs.getString(3),
-                        rs.getDouble(4),
-                        rs.getString(5),
-                        rs.getString(6),
-                        rs.getInt(7),
-                        rs.getString(8),
-                        rs.getString(9)
-                ));
-            }
-            rs.close();
-            ps.close();
-            return arr;
+//public  List<Product> getAll(){
+//        List<Product> arr = new ArrayList<>();
+//        try(Connection connection = ConnectionDB.open()) {
+//            String sql ="select * from product";
+//            PreparedStatement ps = connection.prepareStatement(sql);
+//            ResultSet rs = ps.executeQuery();
+//            while (rs.next()){
+//                arr.add(new Product(
+//                   rs.getString(1),
+//                   rs.getString(2),
+//                        rs.getString(3),
+//                        rs.getDouble(4),
+//                        rs.getString(5),
+//                        rs.getString(6),
+//                        rs.getInt(7),
+//                        rs.getString(8),
+//                        rs.getString(9)
+//                ));
+//            }
+//            rs.close();
+//            ps.close();
+//            return arr;
+//
+//        } catch (SQLException throwables) {
+//            throwables.printStackTrace();
+//        }
+//
+//
+//    return arr;
+//}
 
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
-
-
-        return arr;
-    }
 
     public static void main(String[] args) {
         ProductEntity p = new ProductEntity();
 
-     /*   System.out.println(p.getAll(2, "price", 10));
-        System.out.println(p.getNumberPage());*/
-//        System.out.println(p.addProduct("3", "3", "3", "200000"));
+        System.out.println(p.getAll(2, "price", 10));
+        System.out.println(p.getNumberPage());
     }
 }
