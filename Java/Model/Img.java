a2e4c655ec94400b8d0cd9dfe9ec690c638baff8
DiemMy2909 <73978578+DiemMy2909@users.noreply.github.com>
2021-01-21 11:12:47 +0700

    img

---
+++ Java/Model/Img.java
@@ -0,0 +1,60 @@
+package Model;
+
+public class Img {
+    String id;
+    String img1;
+    String img2;
+    String img3;
+    String img4;
+
+    public Img() {
+    }
+
+    public Img(String id, String img1, String img2, String img3, String img4) {
+        this.id = id;
+        this.img1 = img1;
+        this.img2 = img2;
+        this.img3 = img3;
+        this.img4 = img4;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getImg1() {
+        return img1;
+    }
+
+    public void setImg1(String img1) {
+        this.img1 = img1;
+    }
+
+    public String getImg2() {
+        return img2;
+    }
+
+    public void setImg2(String img2) {
+        this.img2 = img2;
+    }
+
+    public String getImg3() {
+        return img3;
+    }
+
+    public void setImg3(String img3) {
+        this.img3 = img3;
+    }
+
+    public String getImg4() {
+        return img4;
+    }
+
+    public void setImg4(String img4) {
+        this.img4 = img4;
+    }
+}
